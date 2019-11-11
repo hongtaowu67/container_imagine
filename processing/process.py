b1c48e8fdf4eb0474a80e0297f7ab55fa8c256a1
jaydenwu17 <hwu67@jhu.edu>
2019-11-11 13:33:16 -0500

    add write urdf code

--- processing/process.py
+++ processing/process.py
@@ -15,8 +15,11 @@ import subprocess
 
 
 def run_vhacd(vhacd_dir, input_file, output_file, log='log.txt', resolution=500000, depth=20, concavity=0.0025,planeDownsampling=4, 
-        convexhullDownsampling=4, alpha=0.05, beta=0.05, gamma=0.00125,pca=0, mode=0, maxNumVerticesPerCH=256, 
-        minVolumePerCH=0.0001, convexhullApproximation=1, oclDeviceID=2):
+    convexhullDownsampling=4, alpha=0.05, beta=0.05, gamma=0.00125,pca=0, mode=0, maxNumVerticesPerCH=256, 
+    minVolumePerCH=0.0001, convexhullApproximation=1, oclDeviceID=2):
+    """
+    The wrapper function to run the vhacd convex decomposition.
+    """
 
     vhacd_executable = os.path.join(vhacd_dir, 'testVHACD')
     if not os.path.isfile(vhacd_executable):
@@ -35,17 +38,69 @@ def run_vhacd(vhacd_dir, input_file, output_file, log='log.txt', resolution=5000
     process.wait()
     elapsed = time.time() - start_time
 
+    os.remove(log)
+
     print "V-HACD took %d seconds" %(elapsed)
 
 
-# TODO: write urdf file for simulation
-def write_urdf():
-    raise NotImplementedError
+def write_urdf(urdf_path, obj_original_file, obj_vhacd_file, mass=0.0, origin_x=0.0, origin_y=0.0, origin_z=0.0,
+    origin_roll=0.0, origin_pitch=0.0, origin_yaw=0.0, ixx=0.0, ixy=0.0, ixz=0.0, iyy=0.0, iyz=0.0, izz=0.0):
+    """
+    Writing the URDF file for simulation.
+    Args:
+    - obj_original_file: the file name of the original file, e.g., cup_0003.obj
+    - obj_vhacd_file: the filename of the vhacd file, e.g., cup_0003_vhacd.obj
+    """
+    f = open(urdf_path, 'w+')
+    obj_name = obj_original_file.split('.')[0]
+
+    f.write(
+    '<?xml version=\"1.0\" ?>\n'
+    '<robot name=\"%s.urdf\">\n'
+    '  <link name=\"baseLink\">\n'
+    '    <contact>\n'
+    '      <lateral_friction value=\"1.0\"/>\n'
+    '      <inertia_scaling value=\"1.0\"/>\n'
+    '    </contact>\n'
+    '    <inertial>\n'
+    '      <origin rpy=\"0 0 0\" xyz=\"%.6f %.6f %.6f\"/>\n'
+    '      <mass value=\"%.6f\"/>\n'
+    '      <inertia ixx=\"%.6f\" ixy=\"%.6f\" ixz=\"%.6f\" iyy=\"%.6f\" iyz=\"%.6f\" izz=\"%.6f\"/>\n'
+    '    </inertial>\n'
+    '    <visual>\n'
+    '      <origin rpy=\"0 0 0\" xyz=\"0 0 0\"/>\n'
+    '      <geometry>\n'
+    '       <mesh filename=\"%s\" scale=\"1 1 1\"/>\n'
+    '      </geometry>\n'
+    '       <material name=\"white\">\n'
+    '        <color rgba=\"1 1 1 1\"/>\n'
+    '      </material>\n'
+    '    </visual>\n'
+    '    <collision>\n'
+    '      <origin rpy=\"0 0 0\" xyz=\"0 0 0\"/>\n'
+    '      <geometry>\n'
+    '        <mesh filename=\"%s\" scale=\"1 1 1\"/>\n'
+    '      </geometry>\n'
+    '    </collision>\n'
+    '  </link>\n'
+    '</robot>\n' %(obj_name, origin_x, origin_y, origin_z, mass, ixx, ixy, ixz, iyy, iyz, izz, obj_original_file, obj_vhacd_file))
+
+    f.close()
+
+    
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     root_dir = '/home/hongtao/src/cup_imagine'
-    vhacd_dir = os.path.join(root_dir, 'processing/v-hacd/src/build/test/')
+    vhacd_dir = os.path.join(root_dir, 'processing')
     input_file = os.path.join(root_dir, 'model/cup_0003.obj')
     output_file = os.path.join(root_dir,'model/cup_0003_vhacd.obj')
     run_vhacd(vhacd_dir, input_file, output_file)
+
+    urdf_path = os.path.join(root_dir, 'model/cup_0003.urdf')
+    obj_original_file = 'cup_0003.obj'
+    obj_vhacd_file = 'cup_0003_vhacd.obj'
+    write_urdf(urdf_path, obj_original_file, obj_vhacd_file)
+    process_time = time.time() - start_time
+    print("Total process time: ", process_time)
