9dd0e65347aebe473b20b73db1bafb39fd2390f7
jaydenwu17 <hwu67@jhu.edu>
2019-11-26 14:44:38 -0500

    add comments for tsdf_fusion_segmentation.py & utils.py

--- reconstruction/tsdf_fusion_segmentation.py
+++ reconstruction/tsdf_fusion_segmentation.py
@@ -3,8 +3,8 @@
 A wrapper code for running TSDF fusion of several RGBD frame. The base frame is at the ArUco tag.
 After TSDF fusion, the code also segment out the object and save each one of the object.
 TSDF fusion sourced from: https://github.com/andyzeng/tsdf-fusion
-General code sourced from: https://github.com/RobotLocomotion/spartan/blob/854b26e3af75910ef57b874db7853abd4249543e/src/catkin_projects/fusion_server/src/fusion_server/tsdf_fusion.py#L126
-@ author: Hongtao Wu
+
+@author: Hongtao Wu
 Nov 23. 2019
 """
 
@@ -18,7 +18,7 @@ import time
 from skimage import measure
 from plyfile import PlyData, PlyElement
 import array
-from utils import segment_aabb
+from utils import segment_aabb, convert_tsdf_to_ply
 
 
 def run_tsdf_fusion_cuda(tsdf_fusion_dir, image_folder, camera_intrinsics_file, output_dir=None, voxel_grid_origin_x=0.0,
@@ -68,181 +68,19 @@ def run_tsdf_fusion_cuda(tsdf_fusion_dir, image_folder, camera_intrinsics_file, 
     print "tsdf-fusion took %d seconds" %(elapsed)
 
 
-def convert_tsdf_to_ply(tsdf_bin_filename, tsdf_mesh_filename):
+
+def segment_tsdf(tsdf_bin_filename, tsdf_mesh_name, ply_output_prefix, obj_ply_output_prefix):
     """
+    1. Convert tsdf to mesh (ply)
+    2. Segment the mesh into individual objects
     Converts the tsdf binary file to a mesh file in ply format
     The indexing in the tsdf is
     (x,y,z) <--> (x + y * dim_x + z * dim_x * dim_y)
     """
-    start_time = time.time()
-    fin = open(tsdf_bin_filename, "rb")
 
-    tsdfHeader = array.array("f")  # f is the typecode for float32
-    tsdfHeader.fromfile(fin, 8)
-    # print tsdfHeader
-    # print type(tsdfHeader)
+    mesh_points, tsdf_segment, voxelGridOrigin, voxelSize = convert_tsdf_to_ply(tsdf_bin_filename, tsdf_mesh_filename)
 
-    voxelGridDim = tsdfHeader[0:3]
-    voxelGridDim = np.asarray(voxelGridDim, dtype=np.int)
-    voxelGridOrigin = tsdfHeader[3:6]
-    voxelSize = tsdfHeader[6]
-    truncMargin = tsdfHeader[7]
-
-    dim_x = voxelGridDim[0]
-    dim_y = voxelGridDim[1]
-    dim_z = voxelGridDim[2]
-
-
-    headerSize = 8
-    tsdf_vec = np.fromfile(tsdf_bin_filename, np.float32)
-    tsdf_vec = tsdf_vec[headerSize:]
-    tsdf = np.reshape(tsdf_vec, voxelGridDim, order='F') # reshape using Fortran order
-
-    print "tsdf.shape:", tsdf.shape
-    print "voxelGridDim: ", voxelGridDim
-    print "voxeGridOrigin: ", voxelGridOrigin
-    print "tsdf.shape:", tsdf.shape
-
-    verts, faces, normals, values = measure.marching_cubes_lewiner(tsdf, spacing=[voxelSize]*3, level=0)
-
-
-    print "type(verts): ", type(verts)
-    print "verts.shape: ", verts.shape
-    print "faces.shape:", faces.shape
-
-    print "np.max(verts[:,0]): ", np.max(verts[:,0])
-    print "np.min(verts[:,0]): ", np.min(verts[:, 0])
-
-
-    print "verts[0,:] = ", verts[0,:]
-    print "faces[0,:] = ", faces[0,:]
-
-    # transform from voxel coordinates to camera coordinates
-    # note x and y are flipped in the output of marching_cubes
-    mesh_points = np.zeros_like(verts)
-    mesh_points[:,0] = voxelGridOrigin[0] + verts[:,0]
-    mesh_points[:,1] = voxelGridOrigin[1] + verts[:,1]
-    mesh_points[:,2] = voxelGridOrigin[2] + verts[:,2]
-
-    # permute faces to get visualization
-    # faces = np.flip(faces, 1)
-
-
-    # try writing to the ply file
-    print "converting numpy arrays to format for ply file"
-    ply_conversion_start_time = time.time()
-
-    num_verts = verts.shape[0]
-    num_faces = faces.shape[0]
-
-    verts_tuple = np.zeros((num_verts,), dtype=[('x', 'f4'), ('y', 'f4'),
-                                                ('z', 'f4')])
-    faces_tuple = np.zeros((num_faces,), dtype=[('vertex_indices', 'i4', (3,))])
-
-    for i in xrange(0, num_verts):
-        verts_tuple[i] = tuple(mesh_points[i, :])
-
-    for i in xrange(0, num_faces):
-        faces_tuple[i] = faces[i, :].tolist()
-
-    # save it out
-    # try to save it
-    el_verts = PlyElement.describe(verts_tuple, 'vertex')
-    el_faces = PlyElement.describe(faces_tuple, 'face')
-
-    ply_data = PlyData([el_verts, el_faces])
-    print "saving mesh to %s" %(tsdf_mesh_filename)
-    ply = ply_data.write(tsdf_mesh_filename)
-
-    print "converting to ply format and writing to file took %d s" % (time.time() - start_time)
-
-
-def segment_tsdf(tsdf_bin_filename, tsdf_mesh_name, ply_output_file, obj_ply_output_prefix):
-    """
-    Converts the tsdf binary file to a mesh file in ply format
-    The indexing in the tsdf is
-    (x,y,z) <--> (x + y * dim_x + z * dim_x * dim_y)
-    """
-    start_time = time.time()
-    fin = open(tsdf_bin_filename, "rb")
-
-    tsdfHeader = array.array("f")  # f is the typecode for float32
-    tsdfHeader.fromfile(fin, 8)
-    # print tsdfHeader
-    # print type(tsdfHeader)
-
-    voxelGridDim = tsdfHeader[0:3]
-    voxelGridDim = np.asarray(voxelGridDim, dtype=np.int)
-    voxelGridOrigin = tsdfHeader[3:6]
-    voxelSize = tsdfHeader[6]
-    truncMargin = tsdfHeader[7]
-
-    dim_x = voxelGridDim[0]
-    dim_y = voxelGridDim[1]
-    dim_z = voxelGridDim[2]
-
-
-    headerSize = 8
-    tsdf_vec = np.fromfile(tsdf_bin_filename, np.float32)
-    tsdf_vec = tsdf_vec[headerSize:]
-    tsdf = np.reshape(tsdf_vec, voxelGridDim, order='F') # reshape using Fortran order
-
-    tsdf_segment = np.copy(tsdf)
-
-    print "tsdf.shape:", tsdf.shape
-    print "voxelGridDim: ", voxelGridDim
-    print "voxeGridOrigin: ", voxelGridOrigin
-    print "tsdf.shape:", tsdf.shape
-
-    verts, faces, normals, values = measure.marching_cubes_lewiner(tsdf, spacing=[voxelSize]*3, level=0)
-
-
-    print "type(verts): ", type(verts)
-    print "verts.shape: ", verts.shape
-    print "faces.shape:", faces.shape
-
-    print "np.max(verts[:,0]): ", np.max(verts[:,0])
-    print "np.min(verts[:,0]): ", np.min(verts[:, 0])
-
-
-    print "verts[0,:] = ", verts[0,:]
-    print "faces[0,:] = ", faces[0,:]
-
-    # transform from voxel coordinates to camera coordinates
-    # note x and y are flipped in the output of marching_cubes
-    mesh_points = np.zeros_like(verts)
-    mesh_points[:,0] = voxelGridOrigin[0] + verts[:,0]
-    mesh_points[:,1] = voxelGridOrigin[1] + verts[:,1]
-    mesh_points[:,2] = voxelGridOrigin[2] + verts[:,2]
-
-    # try writing to the ply file
-    print "converting numpy arrays to format for ply file"
-    ply_conversion_start_time = time.time()
-
-    num_verts = verts.shape[0]
-    num_faces = faces.shape[0]
-
-    verts_tuple = np.zeros((num_verts,), dtype=[('x', 'f4'), ('y', 'f4'),
-                                                ('z', 'f4')])
-    faces_tuple = np.zeros((num_faces,), dtype=[('vertex_indices', 'i4', (3,))])
-
-    for i in xrange(0, num_verts):
-        verts_tuple[i] = tuple(mesh_points[i, :])
-
-    for i in xrange(0, num_faces):
-        faces_tuple[i] = faces[i, :].tolist()
-
-    # save it out
-    # try to save it
-    el_verts = PlyElement.describe(verts_tuple, 'vertex')
-    el_faces = PlyElement.describe(faces_tuple, 'face')
-
-    ply_data = PlyData([el_verts, el_faces])
-    print "saving mesh to %s" %(tsdf_mesh_filename)
-    ply = ply_data.write(tsdf_mesh_filename)
-
-    print "converting to ply format and writing to file took %d s" % (time.time() - start_time)
-
+    # Segment the mesh points
     objects_aabb = segment_aabb(mesh_points, ply_output_prefix)
     
     for obj_id, obj_aabb in enumerate(objects_aabb):
@@ -285,17 +123,112 @@ def segment_tsdf(tsdf_bin_filename, tsdf_mesh_name, ply_output_file, obj_ply_out
         obj_ply = obj_ply_data.write(obj_mesh_filename)
 
 
+def segment_tsdf_fast(tsdf_bin_filename, tsdf_ply_file, ply_output_prefix, obj_mesh_output_prefix, output_type='obj'):
+    """
+    1. Convert tsdf to mesh (ply)
+    2. Segment the mesh into individual objects
+    Converts the tsdf binary file to a mesh file in ply format
+    The indexing in the tsdf is
+    (x,y,z) <--> (x + y * dim_x + z * dim_x * dim_y)
+    """
+
+    plydata = PlyData.read(tsdf_ply_file)
+    ply_x = plydata['vertex']['x']
+    ply_y = plydata['vertex']['y']
+    ply_z = plydata['vertex']['z']
+    mesh_points = np.zeros((ply_x.shape[0], 3), dtype=np.float32)
+    mesh_points[:, 0] = ply_x
+    mesh_points[:, 1] = ply_y
+    mesh_points[:, 2] = ply_z
+
+    fin = open(tsdf_bin_filename, "rb")
+
+    tsdfHeader = array.array("f")  # f is the typecode for float32
+    tsdfHeader.fromfile(fin, 8)
+    fin.close()
+
+    voxelGridOrigin = tsdfHeader[3:6]
+    voxelSize = tsdfHeader[6]
+    voxelGridDim = tsdfHeader[0:3]
+    voxelGridDim = np.asarray(voxelGridDim, dtype=np.int)
+
+    headerSize = 8
+    tsdf_vec = np.fromfile(tsdf_bin_filename, np.float32)
+    tsdf_vec = tsdf_vec[headerSize:]
+    tsdf = np.reshape(tsdf_vec, voxelGridDim, order='F') # reshape using Fortran order
+
+    # Segment the mesh points
+    objects_aabb = segment_aabb(mesh_points, ply_output_prefix)
+    
+    for obj_id, obj_aabb in enumerate(objects_aabb):
+        min_x_in_voxel = max(int((obj_aabb[0][0] - voxelGridOrigin[0]) / voxelSize) - 3, 0)
+        max_x_in_voxel = min(int((obj_aabb[1][0] - voxelGridOrigin[0]) / voxelSize) + 3, voxelGridDim[0])
+        min_y_in_voxel = max(int((obj_aabb[0][1] - voxelGridOrigin[1]) / voxelSize) - 2, 0)
+        max_y_in_voxel = min(int((obj_aabb[1][1] - voxelGridOrigin[1]) / voxelSize) + 10, voxelGridDim[1])
+        min_z_in_voxel = max(int((obj_aabb[0][2] - voxelGridOrigin[2]) / voxelSize) - 3, 0)
+        max_z_in_voxel = min(int((obj_aabb[1][2] - voxelGridOrigin[2]) / voxelSize) + 3, voxelGridDim[2])
+
+        # Crop the tsdf
+        obj_tsdf = tsdf[min_x_in_voxel:max_x_in_voxel, min_y_in_voxel:max_y_in_voxel, min_z_in_voxel:max_z_in_voxel]
+        # Reconstruct with marching cube
+        obj_verts, obj_faces, obj_normals, obj_values = measure.marching_cubes_lewiner(obj_tsdf, spacing=[voxelSize]*3, level=0)
+
+        obj_mesh_points = np.zeros_like(obj_verts)
+        obj_mesh_points[:,0] = voxelGridOrigin[0] + obj_verts[:,0]
+        obj_mesh_points[:,1] = voxelGridOrigin[1] + obj_verts[:,1]
+        obj_mesh_points[:,2] = voxelGridOrigin[2] + obj_verts[:,2]
+
+        obj_num_verts = obj_verts.shape[0]
+        obj_num_faces = obj_faces.shape[0]
+        
+        # Save PLY file
+        if output_type == 'ply':
+            obj_verts_tuple = np.zeros((obj_num_verts,), dtype=[('x', 'f4'), ('y', 'f4'),
+                                                        ('z', 'f4')])
+            obj_faces_tuple = np.zeros((obj_num_faces,), dtype=[('vertex_indices', 'i4', (3,))])
+
+            for i in xrange(0, obj_num_verts):
+                obj_verts_tuple[i] = tuple(obj_mesh_points[i, :])
+
+            for i in xrange(0, obj_num_faces):
+                obj_faces_tuple[i] = obj_faces[i, :].tolist()
+
+            obj_el_verts = PlyElement.describe(obj_verts_tuple, 'vertex')
+            obj_el_faces = PlyElement.describe(obj_faces_tuple, 'face')
+
+            obj_ply_data = PlyData([obj_el_verts, obj_el_faces])
+            obj_mesh_filename = obj_mesh_output_prefix + '_%d.ply' % (obj_id)
+            obj_ply = obj_ply_data.write(obj_mesh_filename)
+
+        # Save OBJ file
+        if output_type == 'obj':
+            obj_mesh_filename = obj_mesh_output_prefix + '_%d.obj' % (obj_id)
+            f = open(obj_mesh_filename, 'w')
+            for obj_mesh_point in obj_mesh_points:
+                f.write('v {0} {1} {2}\n'.format(obj_mesh_point[0], obj_mesh_point[1], obj_mesh_point[2]))
+            for item in obj_normals:
+                f.write('vn {0} {1} {2}\n'.format(item[0], item[1], item[2]))
+            obj_faces += 1
+            for item in obj_faces:
+                f.write('f {0}//{0} {1}//{1} {2}//{2}\n'.format(item[0], item[1], item[2]))
+            f.close()
+
+
+
+
 # Test
 if __name__ == "__main__":
     tsdf_fusion_dir = "/home/hongtao/src/cup_imagine/reconstruction/tsdf-fusion"
+    model_output_dir = '/home/hongtao/src/cup_imagine/model'
     image_folder = os.path.join(tsdf_fusion_dir, "data/tsdf_data/rgbd-frames")
     camera_intrinsics_file = os.path.join(tsdf_fusion_dir, "data/tsdf_data/camera-intrinsics.txt")
     run_tsdf_fusion_cuda(tsdf_fusion_dir, image_folder, camera_intrinsics_file, 
         voxel_grid_origin_x=-0.3, voxel_grid_origin_y=0.005, voxel_grid_origin_z=-0.13, fast_tsdf_settings=True)
 
-    tsdf_bin_filename = os.path.join(tsdf_fusion_dir, 'model/tsdf.bin')
-    tsdf_mesh_filename = os.path.join(tsdf_fusion_dir, 'model/test_1124_aruco_3.ply')
-    # convert_tsdf_to_ply(tsdf_bin_filename, tsdf_mesh_filename)
-    ply_output_prefix = os.path.join(tsdf_fusion_dir, 'model/test_1124_aruco_3_point')
-    obj_ply_output_prefix = os.path.join(tsdf_fusion_dir, 'model/test_1124_aruco_3_mesh')
-    segment_tsdf(tsdf_bin_filename, tsdf_mesh_filename, ply_output_prefix, obj_ply_output_prefix)
+    tsdf_bin_file = os.path.join(tsdf_fusion_dir, 'model/tsdf.bin')
+    # tsdf_mesh_filename = os.path.join(tsdf_fusion_dir, 'model/test_1126_aruco_1.ply')
+    tsdf_ply_file = os.path.join(tsdf_fusion_dir, 'model/tsdf.ply')
+    ply_output_prefix = os.path.join(tsdf_fusion_dir, 'model/test_1126_aruco_3_point')
+    obj_mesh_output_prefix = os.path.join(model_output_dir, 'test_1126_3_mesh')
+    # segment_tsdf(tsdf_bin_filename, tsdf_mesh_filename, ply_output_prefix, obj_ply_output_prefix)
+    segment_tsdf_fast(tsdf_bin_file, tsdf_ply_file, ply_output_prefix, obj_mesh_output_prefix)
--- reconstruction/utils.py
+++ reconstruction/utils.py
@@ -1,4 +1,19 @@
-#! /usr/bin/env python
+"""
+Utils functions
+
+Segmentation out with plane model and euclidean cluster with PCL.
+Plane model: http://pointclouds.org/documentation/tutorials/planar_segmentation.php#planar-segmentation
+Euclidean cluster: http://pointclouds.org/documentation/tutorials/cluster_extraction.php#cluster-extraction
+
+First use plane model to cut out all the plane. Then segement models with Euclidean cluster.
+Python binding for Plane model: https://github.com/Sirokujira/python-pcl/blob/rc_patches4/examples/official/Segmentation/Plane_model_segmentation.py
+Python binding for Euclidean cluster: https://github.com/Sirokujira/python-pcl/blob/rc_patches4/examples/official/Segmentation/cluster_extraction.py
+
+Convert tsdf to ply source code: https://github.com/RobotLocomotion/spartan/blob/854b26e3af75910ef57b874db7853abd4249543e/src/catkin_projects/fusion_server/src/fusion_server/tsdf_fusion.py#L126
+
+@author: Hongtao Wu
+Nov 25, 2019
+"""
 
 import numpy as np
 import pcl
@@ -50,10 +65,16 @@ def make_rigid_transformation(pos, orn):
 
 def segment_aabb(points, ply_output_prefix):
     """
-    Return the AABB of the segmented object.
+    Return the AABB of the segmented object
+    Trim off the plane and segment the object with Euclidean cluster
+    Save the point cloud of the segmented objects
 
     Args:
     - points (nx3 float array): points from the tsdf
+    - ply_output_prefix: prefix name for the output ply file, e.g., 'test_1124_aruco_3_mesh'
+
+    Returns:
+    - total_aabb: list of aabb of each object
     """
 
     # Load points into a point cloud class
@@ -121,3 +142,99 @@ def segment_aabb(points, ply_output_prefix):
         print 'object aabb: {}'.format(obj_aabb)
 
     return total_aabb
+
+
+def convert_tsdf_to_ply(tsdf_bin_filename, tsdf_mesh_filename):
+    """
+    Converts the tsdf binary file to a mesh file in ply format
+    The indexing in the tsdf is
+    (x,y,z) <--> (x + y * dim_x + z * dim_x * dim_y)
+
+    Args:
+    - tsdf_bin_filename: binary file of the tsdf
+    - tsdf_mesh_filename: output mesh from the tsdf
+
+    Returns:
+    - mesh_points: points on the mesh
+    - tsdf: original tsdf from the binary file
+    - voxelGridOrigin: Origin of the voxel
+    - voxelSize: size of a voxel
+    """
+    start_time = time.time()
+    fin = open(tsdf_bin_filename, "rb")
+
+    tsdfHeader = array.array("f")  # f is the typecode for float32
+    tsdfHeader.fromfile(fin, 8)
+    fin.close()
+
+    voxelGridDim = tsdfHeader[0:3]
+    voxelGridDim = np.asarray(voxelGridDim, dtype=np.int)
+    voxelGridOrigin = tsdfHeader[3:6]
+    voxelSize = tsdfHeader[6]
+    truncMargin = tsdfHeader[7]
+
+    # dim_x = voxelGridDim[0]
+    # dim_y = voxelGridDim[1]
+    # dim_z = voxelGridDim[2]
+
+
+    headerSize = 8
+    tsdf_vec = np.fromfile(tsdf_bin_filename, np.float32)
+    tsdf_vec = tsdf_vec[headerSize:]
+    tsdf = np.reshape(tsdf_vec, voxelGridDim, order='F') # reshape using Fortran order
+
+    # print "tsdf.shape:", tsdf.shape
+    # print "voxelGridDim: ", voxelGridDim
+    # print "voxeGridOrigin: ", voxelGridOrigin
+    # print "tsdf.shape:", tsdf.shape
+
+    verts, faces, normals, values = measure.marching_cubes_lewiner(tsdf, spacing=[voxelSize]*3, level=0)
+
+
+    # print "type(verts): ", type(verts)
+    # print "verts.shape: ", verts.shape
+    # print "faces.shape:", faces.shape
+
+    # print "np.max(verts[:,0]): ", np.max(verts[:,0])
+    # print "np.min(verts[:,0]): ", np.min(verts[:, 0])
+
+
+    print "verts[0,:] = ", verts[0,:]
+    print "faces[0,:] = ", faces[0,:]
+
+    # transform from voxel coordinates to camera coordinates
+    # note x and y are flipped in the output of marching_cubes
+    mesh_points = np.zeros_like(verts)
+    mesh_points[:,0] = voxelGridOrigin[0] + verts[:,0]
+    mesh_points[:,1] = voxelGridOrigin[1] + verts[:,1]
+    mesh_points[:,2] = voxelGridOrigin[2] + verts[:,2]
+
+    # Write the ply file
+    print "Converting numpy arrays to format for ply file"
+    ply_conversion_start_time = time.time()
+
+    num_verts = verts.shape[0]
+    num_faces = faces.shape[0]
+
+    verts_tuple = np.zeros((num_verts,), dtype=[('x', 'f4'), ('y', 'f4'),
+                                                ('z', 'f4')])
+    faces_tuple = np.zeros((num_faces,), dtype=[('vertex_indices', 'i4', (3,))])
+
+    for i in xrange(0, num_verts):
+        verts_tuple[i] = tuple(mesh_points[i, :])
+
+    for i in xrange(0, num_faces):
+        faces_tuple[i] = faces[i, :].tolist()
+
+    # Save the mesh
+    el_verts = PlyElement.describe(verts_tuple, 'vertex')
+    el_faces = PlyElement.describe(faces_tuple, 'face')
+
+    ply_data = PlyData([el_verts, el_faces])
+    print "Saving mesh to %s" %(tsdf_mesh_filename)
+    ply = ply_data.write(tsdf_mesh_filename)
+
+    print "Converting to ply format and writing to file took %d s" % (time.time() - start_time)
+
+    return mesh_points, tsdf, voxelGridOrigin, voxelSize
+
