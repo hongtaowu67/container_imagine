6453b00abd8ff03c4e7b5a54558177f305b25796
jaydenwu17 <hwu67@jhu.edu>
2020-07-07 18:09:26 -0400

    fix some bugs about the image boundary

--- benchmark/crop_image_affnet.py
+++ benchmark/crop_image_affnet.py
@@ -1,3 +1,10 @@
+"""
+Crop the images based on the annotation json for benchmarking with AffordanceNet.
+
+Author: Hongtao Wu
+July 03, 2020
+"""
+
 import cv2
 import json
 import os
@@ -6,10 +13,11 @@ output_folder = "/home/hongtao/Dropbox/ICRA2021/affnet_benchmark/affnet_benchmar
 data_folder = "/home/hongtao/Dropbox/ICRA2021/affnet_benchmark/affnet_benchmark_object"
 total_img_num = 24
 
-class_list = ["bowl"]
+class_list = ["spatula"]
 
 for class_name in class_list:
-    obj_list = ["Ikea_Dinera_Bowl"]
+    # obj_list = ["Ikea_Morgonte_Blue_Bowl", "Origami_Bowl"]
+    obj_list = os.listdir(os.path.join(data_folder, class_name))
     
     for obj_name in obj_list:
         
@@ -32,12 +40,29 @@ for class_name in class_list:
                 img_name = value["filename"]
                 img = cv2.imread(os.path.join(rgbd_dir, img_name))
 
+                print img_name
+                
+                img_h = img.shape[0]
+                img_w = img.shape[1]
+                print "img_h, img_w: ", img_h, img_w
+
                 x = value["regions"][0]["shape_attributes"]["x"]
                 y = value["regions"][0]["shape_attributes"]["y"]
                 width = value["regions"][0]["shape_attributes"]["width"]
                 height = value["regions"][0]["shape_attributes"]["height"]
 
-                crop_img = img[y: (y+height), x:(x+width)]
+                print "x, y: ", x, y
+                print "width, height: ", width, height
+                print "======="
+
+                x_1 = max(0, x)
+                y_1 = max(0, y)
+
+                x_2 = min(x+width, img_w)
+                y_2 = min(y+height, img_h)
+
+
+                crop_img = img[y_1:y_2, x_1:x_2]
                 crop_img_name = img_name.split(".")[0] + ".crop.png"
                 crop_img_path = os.path.join(crop_img_dir, crop_img_name)
                 cv2.imwrite(crop_img_path, crop_img)
