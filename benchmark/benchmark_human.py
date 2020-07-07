cdcd982966fdabbfe53c99e0fbc36e66a6050a02
jaydenwu17 <hwu67@jhu.edu>
2020-07-07 18:10:33 -0400

    add classification with confidence value

--- benchmark/benchmark_human.py
+++ benchmark/benchmark_human.py
@@ -6,6 +6,8 @@ Author: Hongtao Wu
 Mar 20, 2020
 
 Add code to compute the result for all human annotation
+
+Add code to compute the result from map result
 """
 
 from __future__ import division
@@ -17,7 +19,7 @@ import numpy as np
 
 test_classification = True
 check_spill = False
-benchmark_dir = "/home/hongtao/Dropbox/ICRA2021/benchmark/0625_contain_ablation"
+benchmark_dir = "/home/hongtao/Dropbox/ICRA2021/benchmark/test_set_all_imgn_w_tran_0703"
 txt_containability_idx = 3
 txt_spill_idx = 6
 
@@ -99,80 +101,123 @@ with open(os.path.join(annotation_dir, annotation_all_file), 'w') as file1:
             obj_dict[key][3] = False
 
 
-#################### Test Classification and Check Spill ####################
-if test_classification:
+#################### Test Classification with txt ####################
+# if test_classification:
 
-    obj_list = os.listdir(benchmark_dir)
+#     obj_list = os.listdir(benchmark_dir)
 
-    tested_obj = set()
-    correct_rice = 0
-    correct_mm = 0
-    correct_jb = 0
-    correct_db = 0
-    total_tested = 0
-    total_container_mm = 0
-    no_spill = 0
+#     tested_obj = set()
+#     correct_rice = 0
+#     correct_mm = 0
+#     correct_jb = 0
+#     correct_db = 0
+#     total_tested = 0
+#     total_container_mm = 0
+#     no_spill = 0
 
 
-    for obj_txt in obj_list:
-        total_tested += 1
-        obj_name = obj_txt.split('.')[0]
-        tested_obj.add(obj_name)
+#     for obj_txt in obj_list:
+#         total_tested += 1
+#         obj_name = obj_txt.split('.')[0]
+#         tested_obj.add(obj_name)
 
-        obj_path = os.path.join(benchmark_dir, obj_txt)
-        file1 = open(obj_path, 'r')
+#         obj_path = os.path.join(benchmark_dir, obj_txt)
+#         file1 = open(obj_path, 'r')
 
-        file_lines = file1.readlines()
-        obj_containability = file_lines[txt_containability_idx]
+#         file_lines = file1.readlines()
+#         obj_containability = file_lines[txt_containability_idx]
         
-        if "True" in obj_containability:
-            is_container = True
-        elif "False" in obj_containability:
-            is_container = False
-        else:
-            raise ValueError ("The object " + obj_name + " is not a container or a noncontainer!")
+#         if "True" in obj_containability:
+#             is_container = True
+#         elif "False" in obj_containability:
+#             is_container = False
+#         else:
+#             raise ValueError ("The object " + obj_name + " is not a container or a noncontainer!")
 
-        correct_rice += (obj_dict[obj_name][0] == is_container)
-        correct_mm += (obj_dict[obj_name][1] == is_container)
-        correct_jb += (obj_dict[obj_name][2] == is_container)
-        correct_db += (obj_dict[obj_name][3] == is_container)
+#         correct_rice += (obj_dict[obj_name][0] == is_container)
+#         correct_mm += (obj_dict[obj_name][1] == is_container)
+#         correct_jb += (obj_dict[obj_name][2] == is_container)
+#         correct_db += (obj_dict[obj_name][3] == is_container)
 
-        if (obj_dict[obj_name][1] != is_container):
-            print "Containability Incorrect: " + obj_name + " " + str(obj_dict[obj_name][1])
+#         if (obj_dict[obj_name][1] != is_container):
+#             print "Containability Incorrect: " + obj_name + " " + str(obj_dict[obj_name][1])
 
-        if check_spill:
-            gt_obj_is_container_mm = obj_dict[obj_name][1]
-            total_container_mm += gt_obj_is_container_mm
+#         if check_spill:
+#             gt_obj_is_container_mm = obj_dict[obj_name][1]
+#             total_container_mm += gt_obj_is_container_mm
             
-            if gt_obj_is_container_mm:    
-                obj_spill_str = file_lines[txt_spill_idx]
+#             if gt_obj_is_container_mm:    
+#                 obj_spill_str = file_lines[txt_spill_idx]
                 
-                # Convert str to list
-                if "nan" in obj_spill_str:
-                    print "Spill object: {} is not identified as an open container".format(obj_name)   
-                else:
-                    obj_spill_list = ast.literal_eval(obj_spill_str.split(':')[1].split('\n')[0][1:])
-                    obj_spill_list = np.array(obj_spill_list).flatten()
+#                 # Convert str to list
+#                 if "nan" in obj_spill_str:
+#                     print "Spill object: {} is not identified as an open container".format(obj_name)   
+#                 else:
+#                     obj_spill_list = ast.literal_eval(obj_spill_str.split(':')[1].split('\n')[0][1:])
+#                     obj_spill_list = np.array(obj_spill_list).flatten()
 
-                    obj_no_spill = False
-                    for spill in obj_spill_list:
-                        if spill < 7:
-                            obj_no_spill = True
-                            break
+#                     obj_no_spill = False
+#                     for spill in obj_spill_list:
+#                         if spill < 7:
+#                             obj_no_spill = True
+#                             break
 
-                    no_spill += obj_no_spill
+#                     no_spill += obj_no_spill
 
-                    if not obj_no_spill:
-                        print "Spill object: cannot pour into {} ".format(obj_name)            
+#                     if not obj_no_spill:
+#                         print "Spill object: cannot pour into {} ".format(obj_name)    
+
+#     if total_tested != len(tested_obj):
+#         raise ValueError("Some objects have been tested more than once!")
+#     print "Total tested object number: {}".format(len(tested_obj))
+#     print "Contain Rice Accuracy: {}".format(correct_rice / len(tested_obj))
+#     print "Contain M&M Accuracy: {}".format(correct_mm / len(tested_obj))
+#     print "Contain Jelly Beans Accuracy: {}".format(correct_jb / len(tested_obj))
+#     print "Contain Decorative Beads Accuracy: {}".format(correct_db / len(tested_obj))
+
+#     if check_spill:
+#         print "No Spillage Rate: {}".format(no_spill / total_container_mm)
+#################### Test Classification with txt ####################
+
+#################### Test Classification with mAP ####################
+obj_list = os.listdir(benchmark_dir)
+
+tested_obj = set()
+
+correct_mm = 0
+total_tested = 0
+
+for obj_txt in obj_list:
+    total_tested += 1
+    obj_name = obj_txt.split('.')[0]
+    # print "obj_name: ", obj_name
+    tested_obj.add(obj_name)
+
+    obj_path = os.path.join(benchmark_dir, obj_txt)
+    file1 = open(obj_path, 'r')
+
+    file_lines = file1.readlines()
+    obj_containability = float(file_lines[0].split(" ")[1])
+    # print "obj_containability: ", obj_containability
+    
+    if obj_containability > 0.0:
+        is_container = True
+    else:
+        is_container = False
+
+    correct_mm += (obj_dict[obj_name][1] == is_container)
 
 
-    if total_tested != len(tested_obj):
-        raise ValueError("Some objects have been tested more than once!")
-    print "Total tested object number: {}".format(len(tested_obj))
-    print "Contain Rice Accuracy: {}".format(correct_rice / len(tested_obj))
-    print "Contain M&M Accuracy: {}".format(correct_mm / len(tested_obj))
-    print "Contain Jelly Beans Accuracy: {}".format(correct_jb / len(tested_obj))
-    print "Contain Decorative Beads Accuracy: {}".format(correct_db / len(tested_obj))
+    if (obj_dict[obj_name][1] != is_container):
+        print "Containability Incorrect: " + obj_name + " " + str(obj_dict[obj_name][1])
 
-    if check_spill:
-        print "No Spillage Rate: {}".format(no_spill / total_container_mm)
+    
+if total_tested != len(tested_obj):
+    raise ValueError("Some objects have been tested more than once!")
+print "Total tested object number: {}".format(len(tested_obj))
+print "Contain M&M Accuracy: {}".format(correct_mm / len(tested_obj))
+
+       
+#################### Test Classification with mAP ####################
+
+
