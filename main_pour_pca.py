95b95d5c3acf3773fc919794d289a78bd1e765b6
jaydenwu17 <hwu67@jhu.edu>
2020-07-15 12:34:37 -0400

    change object name

--- main_pour_pca.py
+++ main_pour_pca.py
@@ -26,7 +26,7 @@ from pick_and_pour_3 import PickAndPour
 
 cup_urdf = "/home/hongtao/Dropbox/ICRA2021/data/general/cup/Cup_GeoCenter.urdf"
 content_urdf = "/home/hongtao/Dropbox/ICRA2021/data/general/m&m.urdf"
-data_name = "Origami_Pink_Cup"
+data_name = "Ifselect_pan_pour_pca"
 pouring = True
 
 data_root_dir = "/home/hongtao/Dropbox/ICRA2021/data"
