15e5c06235475c9c2cdbe425a01bd9be30210d2a
jaydenwu17 <hwu67@jhu.edu>
2020-07-19 17:50:50 -0400

    add benchmark for restricted test set

--- containability_imagination.py
+++ containability_imagination.py
@@ -10,10 +10,11 @@ import time
 import numpy as np
 from containability.containability_3_1 import Containability
 
+'''
 map_benchmark = True
 content_urdf = "/home/hongtao/Dropbox/ICRA2021/data/general/m&m.urdf"
-obj_dir = "/home/hongtao/Dropbox/ICRA2021/data/test_set_all"
-result_dir = "/home/hongtao/Dropbox/ICRA2021/benchmark/test_set_all_imgn_w_everything_0710"
+obj_dir = "/home/hongtao/Dropbox/ICRA2021/data/test_set_containability"
+result_dir = "/home/hongtao/Dropbox/ICRA2021/benchmark/test_set_containability_w_everything"
 obj_list = os.listdir(obj_dir)
 # obj_list=["Amazon_Name_Card_Holder"]
 
@@ -60,3 +61,59 @@ for obj_name in obj_list:
         with open(result_txt_name, "w") as file1:
             writerow = 'container ' + str(sphere_in_percentage) + ' 0 1 2 3'
             file1.write(writerow)
+'''
+
+# Affnet benchmark
+map_benchmark = True
+content_urdf = "/home/hongtao/Dropbox/ICRA2021/data/general/m&m.urdf"
+data_dir = "/home/hongtao/Dropbox/ICRA2021/affnet_benchmark/affnet_benchmark_object"
+result_dir = "/home/hongtao/Dropbox/ICRA2021/affnet_benchmark/affnet_benchmark_imgn_containability"
+class_list = os.listdir(data_dir)
+# obj_list=["Amazon_Name_Card_Holder"]
+
+# print "Object number: ", len(obj_list)
+
+for class_name  in class_list:
+    class_dir = os.path.join(data_dir, class_name)
+    obj_list = os.listdir(class_dir)
+    for obj_name in obj_list:
+        start_time = time.time()
+        obj_urdf = os.path.join(class_dir, obj_name, obj_name + "_mesh_0.urdf")
+        obj_vhacd_mesh = os.path.join(class_dir, obj_name, obj_name + "_mesh_0_vhacd.obj")
+        C = Containability(obj_urdf, obj_vhacd_mesh, rotate=True, translate=True, friction=True, restitution=True,
+            obj_zero_pos=[0, 0, 0], obj_zero_orn=[0, 0, 0], check_process=False, mp4_dir=None, object_name=obj_name, content_urdf=content_urdf)
+
+        containability_affordance, sphere_in_percentage = C.get_containability()
+        
+        if not map_benchmark:
+            if containability_affordance:
+                drop_spot = C.find_drop_center()
+                print("Dropping at: {}".format(drop_spot))
+            else:
+                drop_spot = [np.nan, np.nan, np.nan]
+        
+        C.disconnect_p()
+
+        imagination_time = time.time() - start_time
+
+        result_txt_name = os.path.join(result_dir, obj_name + ".txt")
+        if not map_benchmark:
+            with open(result_txt_name, "w") as file1:
+                today = time.strftime("%Y-%m-%d-%H-%M", time.localtime())
+                file1.write("Name: " + obj_name + "\n")
+                file1.write("Date: " + today + "\n")
+                file1.write("Content: " + content_urdf.split('/')[-1].split('.')[0] + "\n")
+                file1.write("Containability: " + str(containability_affordance) + "\n")
+                file1.write("Sphere in percentage: " + str(sphere_in_percentage) + "\n")
+                file1.write("Pour position: " + str(list(drop_spot)) + "\n")
+                file1.write("Containability imagination time: " + str(imagination_time) + "\n")
+                file1.write("Object url: \n")
+        
+            # print("------------------")
+            # print(obj_name)
+            # print("Containability: {}".format(containability_affordance))
+            # print("Sphere Percent: {}".format(sphere_in_percentage))
+        else:
+            with open(result_txt_name, "w") as file1:
+                writerow = 'container ' + str(sphere_in_percentage) + ' 0 1 2 3'
+                file1.write(writerow)
