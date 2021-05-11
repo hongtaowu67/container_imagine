52e61a2ab8f0aca4f4b8bb25e40d246717d188f5
hongtaowu67 <hwu67@jhu.edu>
2021-05-11 14:25:29 +0800

    add code for real robot experiment

--- main_pour.py
+++ main_pour.py
@@ -11,56 +11,102 @@
 # Date: June 10, 2020
 
 import os
-import time
-from datetime import date
-
 import numpy as np
 
-# from capture_view import AutoCapture
+from capture_view import AutoCapture
 from processing.tsdf_fusion import run_tsdf_fusion, tsdf_fusion_postprocess
-from processing.process import run_vhacd, write_urdf
+from processing.utils import run_vhacd, write_urdf
 from containability import Containability
 from pouring import CupPour
 from pick_and_pour import PickAndPour
 
-cup_urdf = "object/Cup_GeoCenter.urdf"
-content_urdf = "object/m&m.urdf"
-data_name = "Amazon_Accessory_Tray_pour_pca"
-pouring = True
+parser = argparse.ArgumentParser(description="Real robot pouring experiment")
+parser.add_argument("root_dir",
+                    type=str,
+                    help="root directory of the container imagination")
+parser.add_argument("data_root_dir",
+                    type=str,
+                    help="root directory of the data")
+parser.add_argument("data_name", type=str, help="name of the scene captured")
+parser.add_argument("vhacd_dir", type=str, help="source directory of V-HACD")
+parser.add_argument("-p",
+                    "--pour",
+                    type=bool,
+                    default=False,
+                    help="If set as True, pouring imagination is activated")
+parser.add_argument("-r",
+                    "--robotIP",
+                    type=str,
+                    default=None,
+                    help="The ip address of the UR5 robot")
+parser.add_argument(
+    "-v",
+    "--visualize",
+    type=bool,
+    default=False,
+    help="If set as True, visualization from Pybullet will be activated")
 
-data_root_dir = "data"
+parser.add_argument(
+    "-m",
+    "--mp4",
+    type=str,
+    default=None,
+    help=
+    "If a directory is given, the video of the imagination will be saved to that directory. Need to used with visualization (-v)"
+)
 
-start_time = time.time()
+args = parser.parse_args()
 
-root_dir = os.getcwd()
+# Pouring
+pouring = args.pour
+robot_ip = args.robotIP
+if pouring:
+    print("Pouring imagination and robot pouring is activated...")
+    print("Robot IP address: {}".format(robot_ip))
 
-data_folder = os.path.join(data_root_dir, data_name)
-if not os.path.exists(data_folder):
-    os.mkdir(data_folder)
-    os.mkdir(os.path.join(data_folder, 'rgbd'))
+# Visualization and MP4
+visualization = args.visualize
+if visualization:
+    print("Visualization is activated...")
+
+mp4_dir = args.mp4
+if mp4_dir:
+    print("Imagination Video will be saved in {}".format(mp4_dir))
+
+root_dir = args.root_dir
+cup_urdf = os.path.join(root_dir, "object/Cup_GeoCenter.urdf")
+content_urdf = os.path.join(root_dir, "object/m&m.urdf")
+marker_content_urdf = os.path.join(root_dir, "object/m&m_red.urdf")
+
+vhacd_dir = args.vhacd
+
+data_root_dir = args.data_root_dir
+data_name = args.data_name
+
+data_dir = os.path.join(data_root_dir, data_name)
+if not os.path.exists(data_dir):
+    os.mkdir(data_dir)
+    os.mkdir(os.path.join(data_dir, 'rgbd'))
 
 # Extrinsic camera calibration file
 cam2ee_file = os.path.join(root_dir, "calibrate/camera_pose.txt")
 
 ############### Capture views of the object #################
-# AC = AutoCapture(data_folder=os.path.join(data_folder, 'rgbd'),
-#                  acc=1.0,
-#                  vel=1.0,
-#                  cam2ee_file=cam2ee_file)
-# AC.collect_data()
-
-# autocapture_time = time.time() - start_time
-#############################################################
+AC = AutoCapture(data_folder=os.path.join(data_dir, 'rgbd'),
+                 acc=1.0,
+                 vel=1.0,
+                 cam2ee_file=cam2ee_file)
+AC.collect_data()
 
 ######## 3D reconstruct the object with TSDF Fusion #########
 tsdf_fusion_dir = os.path.join(root_dir, 'processing/TSDFfusion')
 
 # TSDF Fusion
-image_folder = os.path.join(data_root_dir, data_name, 'rgbd')
+
 camera_intrinsics_file = os.path.join(root_dir,
                                       "calibrate/camera-intrinsics.txt")
 run_tsdf_fusion(tsdf_fusion_dir,
-                image_folder,
+                data_dir,
                 camera_intrinsics_file,
                 voxel_grid_origin_x=-0.3,
                 voxel_grid_origin_y=-0.55,
@@ -76,17 +122,16 @@ obj_mesh_output_prefix = os.path.join(data_root_dir, data_name,
                                       data_name + '_mesh')
 tsdf_fusion_postprocess(tsdf_bin_file, tsdf_ply_file, ply_output_prefix,
                         obj_mesh_output_prefix)
-##############################################################
-'''
-##################### VHACD processing #######################
-object_name = data_name + "_mesh_0"
 
+##################### VHACD processing #######################
 # VHACD
-vhacd_dir = os.path.join(root_dir, 'processing')
+object_name = data_name + "_mesh_0"
+# This follows the directory struction from https://github.com/kmammou/v-hacd
+vhacd_executable_dir = os.path.join(vhacd_dir, "src/build/test")
 input_file = os.path.join(data_root_dir, data_name, object_name + '.obj')
 output_file = os.path.join(data_root_dir, data_name,
                            object_name + '_vhacd.obj')
-run_vhacd(vhacd_dir, input_file, output_file)
+run_vhacd(vhacd_executable_dir, input_file, output_file)
 
 # URDF file
 obj_urdf = os.path.join(data_root_dir, data_name, object_name + '.urdf')
@@ -94,14 +139,10 @@ obj_original_file = object_name + '.obj'
 obj_vhacd_file = object_name + '_vhacd.obj'
 write_urdf(obj_urdf, obj_original_file, obj_vhacd_file)
 
-preprocessing_time = time.time() - start_time - autocapture_time
-##############################################################
-
 ################# Containability Imagination #################
-print "Start containability imagination..."
+print("Start containability imagination...")
 obj_vhacd_path = os.path.join(data_root_dir, data_name, obj_vhacd_file)
 
-mp4_dir = os.path.join(data_root_dir, data_name)
 print('URDF: ', obj_urdf)
 
 C = Containability(obj_urdf,
@@ -124,14 +165,10 @@ if containability_affordance:
 else:
     drop_spot = [np.nan, np.nan, np.nan]
 
-containability_imagination_time = time.time(
-) - start_time - autocapture_time - preprocessing_time
-#################################################################
-
 ################### Pouring Imagination ######################
 if pouring:
     if containability_affordance:
-        print "Start pouring imagination..."
+        print("Start pouring imagination...")
         sphere_in_list_se2 = sphere_in_list[:, :2]
         CP = CupPour(cup_urdf,
                      content_urdf,
@@ -145,10 +182,9 @@ if pouring:
                      mp4_dir=mp4_dir,
                      object_name=object_name)
         spill_list = CP.cup_pour()
-        print "Spill List: {}".format(spill_list)
+        print("Spill List: {}".format(spill_list))
 
         imagined_pour_pos, imagined_cup_angle = CP.best_pour_pos_orn()
-        # CP.cup_pour_at(imagined_pour_pos, imagined_cup_angle)
         CP.disconnect_p()
     else:
         spill_list = [[np.nan, np.nan, np.nan], [np.nan, np.nan, np.nan],
@@ -156,46 +192,9 @@ if pouring:
                       [np.nan, np.nan, np.nan], [np.nan, np.nan, np.nan],
                       [np.nan, np.nan, np.nan], [np.nan, np.nan, np.nan]]
 
-pouring_imagination_time = time.time(
-) - start_time - autocapture_time - preprocessing_time - containability_imagination_time
-##############################################################
-
 ################## Real Robot Pouring #####################
 if pouring:
     if containability_affordance:
-        PP = PickAndPour(acc=0.5, vel=0.5)
+        PP = PickAndPour(robot_ip=robot_ip, acc=0.5, vel=0.5)
         PP.pick_vertical()
         PP.pour_multi_orn(imagined_pour_pos, bottle_angle=imagined_cup_angle)
-    else:
-        imagined_pour_pos = [np.nan, np.nan, np.nan]
-        imagined_cup_angle = np.nan
-    pouring_time = time.time(
-    ) - start_time - autocapture_time - preprocessing_time - containability_imagination_time - pouring_imagination_time
-else:
-    imagined_pour_pos = [np.nan, np.nan, np.nan]
-    imagined_cup_angle = np.nan
-    pouring_time = np.nan
-###########################################################
-
-################################################################
-result_txt_name = os.path.join(data_folder, data_name + ".txt")
-with open(result_txt_name, "w") as file1:
-    today = time.strftime("%Y-%m-%d-%H-%M", time.localtime())
-    file1.write("Name: " + data_name + "\n")
-    file1.write("Date: " + today + "\n")
-    file1.write("Containability: " + str(containability_affordance) + "\n")
-    file1.write("Sphere in percentage: " + str(sphere_in_percentage) + "\n")
-    file1.write("Average drop position: " + str(list(drop_spot)) + "\n")
-    file1.write("Imagined pour position: " + str(imagined_pour_pos) + "\n")
-    file1.write("Imagined cup angle: " + str(imagined_cup_angle) + "\n")
-    file1.write("Spill List: " + str(list(spill_list)) + "\n")
-    file1.write("Robot scanning time: " + str(autocapture_time) + "\n")
-    file1.write("Model processing time: " + str(preprocessing_time) + "\n")
-    file1.write("Containability imagination time: " +
-                str(containability_imagination_time) + "\n")
-    file1.write("Pouring imagination time: " + str(pouring_imagination_time) +
-                "\n")
-    file1.write("Pouring time: " + str(pouring_time) + "\n")
-    file1.write("Object url: \n")
-################################################################
-'''
